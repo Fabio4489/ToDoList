6f9dd9fbae224f9d8cf1fca139655e8936e8e20d
Fabio4489 <fabiodangelo2809@gmail.com>
2025-10-30 11:14:58 +0100

    Update codice, modifica dell'eliminazione e inserimento, + aggiunta dell'addEventListener

--- main.js
+++ main.js
@@ -1,57 +1,47 @@
-let lista = [];
-
 stampa();
+
+document.querySelector(".js-btn-salva").addEventListener('click', () => {
+    salva();
+});
 
 function salva(){
     let todo = document.getElementById('todo').value;
     document.getElementById('todo').value = "";
 
-    if(todo != ""){
-        let antonio = localStorage.getItem("LISTA");
+    if(todo === "") alert("ERRORE!!! Inserire un todo valido");
+    
+    let lista = localStorage.getItem("LISTA") || [];
 
-        if (antonio == null){
-            lista.push(todo);
-            localStorage.setItem("LISTA", JSON.stringify(lista));
-            stampa();
-
-        }else{
-            antonio = JSON.parse(antonio);
-            antonio.push(todo);
-            localStorage.setItem("LISTA", JSON.stringify(antonio));
-            stampa();
-
-        }
-
-    }else{
-        alert("ERRORE!!! Inserire un todo valido");
-
-    }
-}
-
-function cancella(x){
-    let laListaStr = localStorage.getItem("LISTA");
-    let laLista = JSON.parse(laListaStr);
-    let laListaFiltrata = laLista.filter(e => e != laLista[x]);
-    localStorage.removeItem('LISTA');
-    localStorage.setItem("LISTA", JSON.stringify(laListaFiltrata));
-    stampa(); 
-
+    lista = JSON.parse(lista);
+    lista.push(todo);
+    localStorage.setItem("LISTA", JSON.stringify(lista));
+    stampa();
 }
 
 function stampa(){
     let laListaStr = localStorage.getItem("LISTA");
-    let laLista = JSON.parse(laListaStr);
+    let laLista = JSON.parse(laListaStr) || [];
 
-    if(laLista.length != 0){
-        let laLista = JSON.parse(laListaStr);
-        let SB = '';
-        for(let i=0; i<laLista.length; i++){
-            SB = SB + `<div class="grid"><div class="todo">` + String(laLista[i]) + `</div><button onclick="cancella(`+[i]+`)">Cancella todo</button></div>`;
-        };        
-        document.querySelector('.js-lista').innerHTML = SB;
-
-    }else{
+    if(laLista.length === 0){
         document.querySelector('.js-lista').innerHTML = '<p>Ancora nessun todo</p>';
-
+        return;
     }
+    let SB = '';
+    for(let i=0; i<laLista.length; i++){
+        SB = SB + `
+        <div class="grid">
+            <div class="todo">` + String(laLista[i]) + `</div>
+            <button class="js-btn-elimina">Cancella todo</button>
+        </div>`;
+    };        
+    document.querySelector('.js-lista').innerHTML = SB;
+    
+    document.querySelectorAll(".js-btn-elimina").forEach((btn, indice) => {
+        btn.addEventListener('click', () => {
+            // cancella(laLista, indice);
+            laLista.splice(indice, 1)
+            localStorage.setItem("LISTA", JSON.stringify(laLista));
+            stampa();
+        });
+    });
 }
