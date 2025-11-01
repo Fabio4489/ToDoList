90a9bc95e807af30f304ad2b2ac34595ff75bb3d
Fabio4489 <fabiodangelo2809@gmail.com>
2025-11-01 12:06:47 +0100

    Revisione dei null provenienti dal local storage

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ document.querySelector(".js-btn-salva").addEventListener('click', () => {
     salva();
 });
 
+function localS(){
+    let lista = localStorage.getItem("LISTA");
+
+}
+
 function salva(){
     let todo = document.getElementById('todo').value;
     document.getElementById('todo').value = "";
 
     if(todo === "") alert("ERRORE!!! Inserire un todo valido");
     
-    let lista = localStorage.getItem("LISTA") || [];
+    let lista = localStorage.getItem("LISTA") || "[]";
 
-    lista = JSON.parse(lista);
+
+    if(lista != null)
+        lista = JSON.parse(lista);
+
     lista.push(todo);
     localStorage.setItem("LISTA", JSON.stringify(lista));
     stampa();
@@ -20,28 +28,31 @@ function salva(){
 
 function stampa(){
     let laListaStr = localStorage.getItem("LISTA");
-    let laLista = JSON.parse(laListaStr) || [];
-
-    if(laLista.length === 0){
+    
+    if(laListaStr != null){
+        let laLista = JSON.parse(laListaStr) || '[]';
+        
+        if(laLista.length === 0){
         document.querySelector('.js-lista').innerHTML = '<p>Ancora nessun todo</p>';
         return;
-    }
-    let SB = '';
-    for(let i=0; i<laLista.length; i++){
-        SB = SB + `
-        <div class="grid">
+        }
+        let SB = '';
+        for(let i=0; i<laLista.length; i++){
+            SB = SB + `
+            <div class="grid">
             <div class="todo">` + String(laLista[i]) + `</div>
             <button class="js-btn-elimina">Cancella todo</button>
-        </div>`;
-    };        
-    document.querySelector('.js-lista').innerHTML = SB;
-    
-    document.querySelectorAll(".js-btn-elimina").forEach((btn, indice) => {
-        btn.addEventListener('click', () => {
-            // cancella(laLista, indice);
-            laLista.splice(indice, 1)
-            localStorage.setItem("LISTA", JSON.stringify(laLista));
-            stampa();
+            </div>`;
+        };        
+        document.querySelector('.js-lista').innerHTML = SB;
+        
+        document.querySelectorAll(".js-btn-elimina").forEach((btn, indice) => {
+            btn.addEventListener('click', () => {
+                // cancella(laLista, indice);
+                laLista.splice(indice, 1)
+                localStorage.setItem("LISTA", JSON.stringify(laLista));
+                stampa();
+            });
         });
-    });
+    }
 }
